2eec66ad88ea3eba08973d13478d8f11888f7306
Yassir Irfan <59363528+yassirirfan@users.noreply.github.com>
2022-06-02 17:36:29 +0530

    Second Commit

--- Yassir/Python/sample.py
+++ Yassir/Python/sample.py
@@ -30,30 +30,36 @@ while True:
         pos_count = 0
         i = -1      #last position index
         len1 = 4    #length of valid number
+        rabs = ''  
         while len1 != 0 :
             d1 = temp1[i]
             d2 = temp2[i]
-            if(d1 == d2): pos_count += 1
+            if(d1 == d2): 
+                pos_count += 1
+                rabs += d1
             len1 -= 1
             i -= 1
         if(pos_count >  0): print("You got rabbit {0} time(s) !".format(pos_count))
 
-        t_temp = user_num #Tortoise section --> [Guessed Number has a number same somewhere in Random Num] [39 - 47]
+         #Tortoise section --> [Guessed Number has a number same somewhere in Random Num] [39 - 47]
         j = -1
         l2 = 4
         count = 0
         while l2 != 0 :
-            if(t_temp[j]  in rand):count += 1
+            if(user_num[j]  in rand ):
+                if user_num[j] not in rabs:
+                        count += 1
             l2 -= 1
             j -= 1
-        if count > 0 : print("You have got tortoise {0} times".format(count))
+        if count > 0 : print("You have got tortoise {0} times ".format(count))
 
+    if count == 0 and pos_count == 0: print("Sorry You got Nothing")
     print("Computer Generated number was : ",rand) #Test section inorder to track program efficiency --> [Not Necessary] [49]
 
     ch = input("Do you want to continue ? (y/n) ")
     while ch not in ('y','n'): 
         print("Invalid, Please enter either y or n")
-        ch = input("Do you want to continue ? (y/n) ")
+        ch = input("Do you want to continue ? (y/n)")
         
     if ch == 'y' and win == 1:rand = genRand()
     elif ch == 'n' : break
---
+++ Yassir/Python/test.py
@@ -0,0 +1,6 @@
+from binascii import a2b_base64
+
+
+
+a = '123'.replace('3','')
+print(a)
